fa7e166b951c6ce0f073eb079bf7ea07ec5bee4a
Jamil Abreu <abreu.jamil@gmail.com>
2013-11-19 16:44:03 -0500

    precompile js

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Thirdplace::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( imagesloaded.min.js infinite_scroll.min.js packery.min.js users.js )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
